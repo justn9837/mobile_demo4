d60c04d3cf83cb727ae86e2e000d52ab2aa7a9fa
justn9837 <justn9837@gmail.com>
2025-11-19 01:04:26 +0700

    Delete try11 directory

--- try11/lib/app/modules/home/bindings/home_binding.dart
+++
@@ -1,6 +0,0 @@
-import '../controllers/home_controller.dart';
-
-class HomeBinding {
-  // simple binding wrapper to create controller instance
-  final HomeController controller = HomeController();
-}
--- try11/lib/app/routes/app_routes.dart
+++
@@ -1,6 +0,0 @@
-class AppRoutes {
-  static const String login = '/';
-  static const String register = '/register';
-  static const String home = '/home';
-  static const String dosenHome = '/dosen_home';
-}
